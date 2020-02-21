b111da1b96f3a197a93cd8137536dee4444a1ac4
litong <ghostong@126.com>
2020-02-22 00:42:54 +0800

    修改自动加载问题

--- src/LitMsFunction.php
+++ src/LitMsFunction.php
@@ -2,7 +2,7 @@
 //载入模块
 function Model ( $modelName, $instantiate = false ) {
     $className = $modelName."Model";
-    require_once ($className.".php");
+    spl_autoload($className);
     if($instantiate) {
         return new $className;
     }else{
